cd30cc041b0091c415bc58131518d056389b119b
Lera Kravets <kravetsvaleryia@gmail.com>
2020-03-23 20:12:55 +0300

    feat: did Task6

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,30 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    NSArray *fibArray = @[@0, @1, @1, @2, @3, @5, @8, @13, @21, @34, @55, @89, @144, @233];
+    NSInteger value = 0;
+    NSInteger index = 0;
+    NSInteger fib1 = 0;
+    NSInteger fib2 = 0;
+    NSInteger fib3 = 0;
+    NSInteger prod = [number intValue];
+
+    while (value < prod) {
+        index = index + 1;
+        fib1 = [fibArray[index] intValue];
+        fib2 = [fibArray[index+1] intValue];
+        fib3 = [fibArray[index+2] intValue];
+        value = fib1 * fib2;
+    }
+
+    if (value == prod) {
+        NSNumber *number1 = [NSNumber numberWithInteger: fib1];
+        NSNumber *number2 = [NSNumber numberWithInteger: fib2];
+        return @[number1, number2, @1];
+    } else {
+        NSNumber *number1 = [NSNumber numberWithInteger: fib1];
+        NSNumber *number2 = [NSNumber numberWithInteger: fib2];
+        return @[number1, number2, @0];
+    }
 }
 @end
